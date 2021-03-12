55555a3d1aa187b2c5c6dbada8199a6c38655267
Leo Rondeau <christopherleorondeau@gmail.com>
2021-03-12 11:58:26 -0600

    changed customer from eater

--- nashvillehotindexapi/models/customer.py
+++ nashvillehotindexapi/models/customer.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Eater(models.Model):
+class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     user_name = models.CharField(max_length=50)
     heat_tolerance = models.IntegerField(null=True, blank=True, default=None)
