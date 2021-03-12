9abbfc4718d2c9c53252d2fb93ba2a2a1e66fa80
Leo Rondeau <christopherleorondeau@gmail.com>
2021-03-12 11:19:55 -0600

    deleted tables that are now merged into other tables

--- nashvillehotindexapi/models/eater.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Eater(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_name = models.CharField(max_length=50)
-    heat_tolerance = models.IntegerField(null=True, blank=True, default=None)
--- nashvillehotindexapi/models/note.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Note(models.Model):
-    restaurant = models.ForeignKey("Restaurant", on_delete=models.CASCADE)
-    eater = models.ForeignKey("Eater", on_delete=models.CASCADE)
-    note = models.CharField(max_length=250)
