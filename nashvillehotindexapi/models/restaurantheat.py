a1aa860cbc5feec3bc7c5f947afae87309c2dc97
Leo Rondeau <christopherleorondeau@gmail.com>
2021-03-12 11:58:49 -0600

    change heat to restaurant heat

--- nashvillehotindexapi/models/restaurantheat.py
+++ nashvillehotindexapi/models/restaurantheat.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class RestaurantHeat(models.Model):
     restaurant = models.ForeignKey("Restaurant", on_delete=models.CASCADE)
-    name = models.ForeignKey("Heat", on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
