64419368183b852edad990349a5c43f1450d5760
Leo Rondeau <christopherleorondeau@gmail.com>
2021-03-13 18:41:50 -0600

    Viewset for List and Retrieve for restaurant heats

--- nashvillehotindexapi/models/restaurantheat.py
+++ nashvillehotindexapi/models/restaurantheat.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class RestaurantHeat(models.Model):
-    restaurant = models.ForeignKey("Restaurant", on_delete=models.CASCADE)
+    restaurant = models.ForeignKey("Restaurant", on_delete=models.CASCADE, related_name="heatlevels")
     name = models.CharField(max_length=50)
