d96a8b7540f46eb83c5669132374e124467f5231
Leo Rondeau <christopherleorondeau@gmail.com>
2021-03-13 18:42:44 -0600

    added order.py and restaurant heat.py

--- nashvillehotindexapi/views/restaurantheat.py
+++ nashvillehotindexapi/views/restaurantheat.py
@@ -5,12 +5,12 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
-from nashvillehotindexapi.models import Restaurant
 from nashvillehotindexapi.models import RestaurantHeat
+from nashvillehotindexapi.models import Restaurant
 
-class Restaurants(ViewSet):
+class RestaurantHeats(ViewSet):
 
-    
+
     def retrieve(self, request, pk=None):
         """Handle GET requests for single game
 
@@ -23,9 +23,9 @@ class Restaurants(ViewSet):
             #   http://localhost:8000/games/2
             #
             # The `2` at the end of the route becomes `pk`
-            restaurant = Restaurant.objects.get(pk=pk)
+            restaurantheat = RestaurantHeat.objects.get(pk=pk)
             # restaurant_heat_levels = RestaurantHeat.objects.filter(restaurant=restaurant)
-            serializer = RestaurantSerializer(restaurant, context={'request': request})
+            serializer = RestaurantHeatSerializer(restaurantheat, context={'request': request})
             return Response(serializer.data)
         except Exception as ex:
             return HttpResponseServerError(ex, status=status.HTTP_404_NOT_FOUND) 
@@ -37,7 +37,7 @@ class Restaurants(ViewSet):
             Response -- JSON serialized list of games
         """
         # Get all game records from the database
-        restaurants = Restaurant.objects.all()
+        restaurants = RestaurantHeat.objects.all()
 
         # Support filtering games by type
         #    http://localhost:8000/games?type=1
@@ -45,10 +45,9 @@ class Restaurants(ViewSet):
         # That URL will retrieve all tabletop games
         
 
-        serializer = RestaurantSerializer(
+        serializer = RestaurantHeatSerializer(
             restaurants, many=True, context={'request': request})
         return Response(serializer.data)
-
 
 class RestaurantSerializer(serializers.ModelSerializer):
     """JSON serializer for games
@@ -58,10 +57,24 @@ class RestaurantSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = Restaurant
+        fields = ('id', 'name')
+
+
+
+class RestaurantHeatSerializer(serializers.ModelSerializer):
+    """JSON serializer for Restaurant heat levels
+    
+
+    Arguments:
+        serializer type
+    """
+    restaurant = RestaurantSerializer(many=False)
+    
+    class Meta:
+        model = RestaurantHeat
         url = serializers.HyperlinkedIdentityField(
             view_name='restaurant',
             lookup_field='id'
         )
-        fields = ('id', 'name', 'website', 'img', 'heatlevels')
+        fields = ('id', 'name', 'restaurant')
         depth = 1
-
