eea2545721beeede82f92e105b415e517ae30710
Leo Rondeau <christopherleorondeau@gmail.com>
2021-03-13 18:41:10 -0600

    added url for restaurant, heat and order

--- nashvillehotindex/urls.py
+++ nashvillehotindex/urls.py
@@ -4,9 +4,13 @@ from django.conf.urls import include
 from django.urls import path
 from nashvillehotindexapi.views import register_user, login_user
 from nashvillehotindexapi.views import Restaurants
+from nashvillehotindexapi.views import RestaurantHeats
+from nashvillehotindexapi.views import Orders
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'restaurants', Restaurants, 'restaurant')
+router.register(r'restaurantheats', RestaurantHeats, 'restaurantheat')
+router.register(r'orders', Orders, 'order')
 
 urlpatterns = [
     path('', include(router.urls)),
