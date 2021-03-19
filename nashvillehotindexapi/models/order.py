159f36af41e00632818b961b8792543317ab1937
Leo Rondeau <christopherleorondeau@gmail.com>
2021-03-18 21:20:42 -0500

    added a datefield

--- nashvillehotindexapi/models/order.py
+++ nashvillehotindexapi/models/order.py
@@ -5,5 +5,6 @@ class Order(models.Model):
     restaurant = models.ForeignKey("Restaurant", on_delete=models.CASCADE)
     customer = models.ForeignKey("Customer", on_delete=models.CASCADE)
     restaurantheat = models.ForeignKey("RestaurantHeat", on_delete=models.CASCADE)
+    created_date = models.DateField(default="0000-00-00",)
     note = models.CharField(max_length=150, null=True)
     enjoyable = models.BooleanField(default=True, null=True)
