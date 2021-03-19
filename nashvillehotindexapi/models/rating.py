2c34dae252f9b323ab94dcd23f391d9518f66746
Leo Rondeau <christopherleorondeau@gmail.com>
2021-03-18 22:16:38 -0500

    added related name but will not use

--- nashvillehotindexapi/models/rating.py
+++ nashvillehotindexapi/models/rating.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Rating(models.Model):
-    restaurantheat = models.ForeignKey("RestaurantHeat", on_delete=models.CASCADE)
+    restaurantheat = models.ForeignKey("RestaurantHeat", on_delete=models.CASCADE, related_name="ratings")
     customer = models.ForeignKey("Customer", on_delete=models.CASCADE)
     rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)],)
