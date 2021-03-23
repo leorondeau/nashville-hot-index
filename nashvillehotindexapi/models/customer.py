8b011e9b7c6fe47c4fe2736bbd6a8ae8fa047d85
Leo Rondeau <christopherleorondeau@gmail.com>
2021-03-23 16:40:23 -0500

    removed name

--- nashvillehotindexapi/models/customer.py
+++ nashvillehotindexapi/models/customer.py
@@ -4,5 +4,4 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_name = models.CharField(max_length=50)
     heat_tolerance = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)],)
