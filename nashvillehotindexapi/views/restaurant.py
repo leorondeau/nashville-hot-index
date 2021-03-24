88e5f9349621fd11c39eb937fbf24bbe7f35d2e9
Leo Rondeau <christopherleorondeau@gmail.com>
2021-03-24 11:45:14 -0500

    wrote heat suggestion logic

--- nashvillehotindexapi/views/restaurant.py
+++ nashvillehotindexapi/views/restaurant.py
@@ -8,6 +8,8 @@ from rest_framework import status
 from nashvillehotindexapi.models import Restaurant
 from nashvillehotindexapi.models import RestaurantHeat
 
+from nashvillehotindexapi.models import Customer
+
 class Restaurants(ViewSet):
 
     
@@ -22,9 +24,23 @@ class Restaurants(ViewSet):
             # Django parses it from the URL route parameter
             #   http://localhost:8000/games/2
             #
-            # The `2` at the end of the route becomes `pk`
+            # The `2` at the end of he route becomes `pk
+            
             restaurant = Restaurant.objects.get(pk=pk)
-            # restaurant_heat_levels = RestaurantHeat.objects.filter(restaurant=restaurant)
+            customer = Customer.objects.get(user=request.auth.user)
+            restaurant_heat_levels = RestaurantHeat.objects.filter(restaurant=restaurant)
+            restaurant.suggested_heat = "mild"
+
+            for heat_level in restaurant_heat_levels:
+                if int(heat_level.average_rating) == (customer.heat_tolerance):
+                    restaurant.suggested_heat = heat_level.name
+                elif int(heat_level.average_rating) == (customer.heat_tolerance - 1):
+                    restaurant.suggested_heat = heat_level.name
+                elif int(heat_level.average_rating) == (customer.heat_tolerance + 1):
+                    restaurant.suggested_heat = heat_level.name
+                
+
+
             serializer = RestaurantSerializer(restaurant, context={'request': request})
             return Response(serializer.data)
         except Exception as ex:
@@ -46,6 +62,8 @@ class Restaurants(ViewSet):
         except ValueError as ex:
             return HttpResponseServerError(ex, status=status.HTTP_404_NOT_FOUND) 
 
+    
+
 
 class RestaurantSerializer(serializers.ModelSerializer):
     """JSON serializer for games
@@ -59,6 +77,6 @@ class RestaurantSerializer(serializers.ModelSerializer):
             view_name='restaurant',
             lookup_field='id'
         )
-        fields = ('id', 'name', 'website', 'img', 'heatlevels')
+        fields = ('id', 'name', 'website', 'img', 'heatlevels', 'suggested_heat')
         depth = 1
 
