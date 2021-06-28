918956298e146886c81c69c01c503c7970fc78f6
Leo Rondeau <christopherleorondeau@gmail.com>
2021-06-28 15:21:08 -0500

    updated img column to react path

--- nashvillehotindex.session.sql
+++ nashvillehotindex.session.sql
@@ -1,10 +1,2 @@
-update nashvillehotindexapi_restaurant
-set img = '/static/media/boltons.85800d15.png'
-where id = 1;
 
-
-
-update nashvillehotindexapi_restaurant
-set img = '/static/media/firecracker.4d29ce76.png'
-where id = 11;
 
