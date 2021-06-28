88768e200e4a60ce1011263208ff435bc7e5a06c
Leo Rondeau <christopherleorondeau@gmail.com>
2021-06-28 13:01:05 -0500

    update query for hattieb image

--- nashvillehotindex.session.sql
+++ nashvillehotindex.session.sql
@@ -1,4 +1,10 @@
 update nashvillehotindexapi_restaurant
-set img = '/Users/admin/workspace/nashvillehotindexreact/src/images/nashvillehotlogos/boltons.png'
+set img = '/static/media/boltons.85800d15.png'
 where id = 1;
+
+
+
+update nashvillehotindexapi_restaurant
+set img = '/static/media/princes.0c36b8c3.webp'
+where id = 4;
 
